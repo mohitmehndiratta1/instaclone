310f5538887c958393c6238f155d0b6d227adbc0
mohitmehndiratta <mohit.esteem@gmail.com>
2017-07-29 10:25:21 +0530

    instaclone

--- instaclone/myapp/models.py
+++ instaclone/myapp/models.py
@@ -1,44 +1,56 @@
-from django.db import models
+from __future__ import unicode_literals
 
+from django.db import models
 import uuid
 
-# Create your models here.
-
-
 class UserModel(models.Model):
-	email = models.EmailField()
-	name = models.CharField(max_length=120)
-	username = models.CharField(max_length=120)
-	password = models.CharField(max_length=40)
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+    email = models.EmailField(max_length=254, default=0)
+    name = models.CharField(max_length=120, default=0)
+    username = models.CharField(max_length=120,default=0)
+    password = models.CharField(max_length=40, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
 class SessionToken(models.Model):
-	user = models.ForeignKey(UserModel)
-	session_token = models.CharField(max_length=255)
-	last_request_on = models.DateTimeField(auto_now=True)
-	created_on = models.DateTimeField(auto_now_add=True)
-	is_valid = models.BooleanField(default=True)
+    user = models.ForeignKey(UserModel)
+    session_token = models.CharField(max_length=240)
+    created_on = models.DateTimeField(auto_now_add=True)
+    is_valid = models.BooleanField(default=True)
 
-	def create_token(self):
-		self.session_token = uuid.uuid4()
+    def create_token(self):
+        self.session_token = uuid.uuid4()
 
-class post(models.Model):
-	user = models.ForeignKey(UserModel)
-	image = models.FileField(upload_to='user_images')
-	image_url = models.CharField(max_length=255)
-	caption = models.CharField(max_length=240)
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+class PostModel(models.Model):
+  user = models.ForeignKey(UserModel)
+  image = models.FileField(upload_to='user_images')
+  image_url = models.CharField(max_length=255)
+  caption = models.CharField(max_length=240)
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+  has_liked = False
 
-class LikeModel(models.Model):
-	user = models.ForeignKey(UserModel)
-	post = models.ForeignKey(post)
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+  @property
+  def like_count(self):
+      return len(PostLikeModel.objects.filter(post=self))
+
+  @property
+  def comments(self):
+      return PostCommentModel.objects.filter(post=self).order_by('created_on')
 
 
-class LikeForm(forms.ModelForm):
-	class Meta:
-		model = LikeModel
-		fields = ['post']
+
+class PostLikeModel(models.Model):
+    user = models.ForeignKey(UserModel)
+    post = models.ForeignKey(PostModel)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class PostCommentModel(models.Model):
+  user = models.ForeignKey(UserModel)
+  post = models.ForeignKey(PostModel)
+  comment_text = models.CharField(max_length=555)
+  created_on = models.DateTimeField(auto_now_add=True)
+  updated_on = models.DateTimeField(auto_now=True)
+
+
